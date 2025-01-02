2f8ef41b5a3f07041bcb3a37baa793523988797e
Quan Pham <6479147+quanphm@users.noreply.github.com>
2025-01-02 16:48:15 +0700

    form action

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,5 +1,7 @@
+import { userKeys } from "@/services/query-key-factory";
 import { usersQueryOptions } from "@/services/query-options";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { createUser } from "@/services/server-fn";
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -10,14 +12,27 @@ export const Route = createFileRoute("/")({
 });
 
 function RouteComponent() {
+	const queryClient = useQueryClient();
 	const { data } = useSuspenseQuery(usersQueryOptions());
+
+	async function formAction(formData: FormData) {
+		await createUser({ data: formData });
+		queryClient.invalidateQueries({
+			queryKey: userKeys.all,
+		});
+	}
 
 	return (
 		<div>
-			<button type="button">Add user</button>
 			{data.map((u: any) => (
 				<p key={u.id}>{u.username}</p>
 			))}
+
+			<form action={formAction}>
+				<input name="username" />
+				<input name="email" />
+				<button type="submit">Submit</button>
+			</form>
 		</div>
 	);
 }
--- apps/web/src/services/server-fn.ts
+++ apps/web/src/services/server-fn.ts
@@ -5,6 +5,7 @@
 import { apiClient } from "@/utils/http-client";
 import { notFound } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/start";
+import { setResponseStatus } from "vinxi/http";
 
 export const fetchUsers = createServerFn({ method: "GET" }).handler(async () => {
 	try {
@@ -17,3 +18,22 @@ export const fetchUsers = createServerFn({ method: "GET" }).handler(async () => 
 		throw err;
 	}
 });
+
+export const createUser = createServerFn({ method: "POST" })
+	.validator((data: FormData) => {
+		const username = data.get("username");
+		const email = data.get("email");
+		return {
+			username,
+			email,
+		};
+	})
+	.handler(async ({ data }) => {
+		try {
+			const result = await apiClient.post("/users", data).then((r) => r.data);
+			setResponseStatus(201);
+			return result;
+		} catch (err) {
+			throw err;
+		}
+	});
