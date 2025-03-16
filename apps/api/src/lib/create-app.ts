6992ba675f6ebcab0b41f329f6bf50450064b3ff
Quan Pham <6479147+quanphm@users.noreply.github.com>
2025-03-16 15:32:00 +0700

    logger

--- apps/api/src/lib/create-app.ts
+++ apps/api/src/lib/create-app.ts
@@ -16,7 +16,7 @@ export function createApp() {
 	app.use(etag());
 	app.use(
 		logger({
-			enabled: false,
+			enabled: process.env.NODE_ENV === "production",
 			pretty: process.env.NODE_ENV === "development",
 			excludePaths: ["/docs", "/openapi"],
 		}),
