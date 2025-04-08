5089280a6ff503d51dfdfdb6589a7a3bdd5621f8
Quan Pham <6479147+quanphm@users.noreply.github.com>
2025-04-08 08:26:45 +0700

    save

--- apps/app/src/atoms/expense.ts
+++ apps/app/src/atoms/expense.ts
@@ -1,18 +1,31 @@
 import type { ExpenseFormSchema } from "@/lib/schema";
+import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
-export const draftExpenseAtom = atomWithStorage<Omit<ExpenseFormSchema, "attachments">>(
-	"draft-expense",
-	{
-		title: "",
-		description: "",
-		date: new Date().toISOString(),
-		transaction: {
-			value: 0,
-			currency: "",
-		},
-		walletId: "",
-		categoryId: "",
-		repeat: "one-time",
+type ExpenseAtomSchema = Omit<ExpenseFormSchema, "attachments">;
+
+export const draftExpenseAtom = atomWithStorage<ExpenseAtomSchema>("draft-expense", {
+	title: "",
+	description: "",
+	date: new Date().toISOString(),
+	transaction: {
+		value: 0,
+		currency: "",
 	},
-);
+	walletId: "",
+	categoryId: "",
+	repeat: "one-time",
+});
+
+export const selectedExpenseAtom = atom<ExpenseAtomSchema>({
+	title: "",
+	description: "",
+	date: new Date().toISOString(),
+	transaction: {
+		value: 0,
+		currency: "",
+	},
+	walletId: "",
+	categoryId: "",
+	repeat: "one-time",
+});
