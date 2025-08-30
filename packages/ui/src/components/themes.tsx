fed354db8e148bf96cfed17f342d58f6f255e744
Quan Pham <6479147+quanphm@users.noreply.github.com>
2025-08-30 10:03:05 +0700

    remove unused codes

--- packages/ui/src/components/themes.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-export function Theme({ children }: { children: React.ReactNode }) {
-	return (
-		<NextThemesProvider defaultTheme="light" attribute="class" disableTransitionOnChange>
-			{children}
-		</NextThemesProvider>
-	);
-}
