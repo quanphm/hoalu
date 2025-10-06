477113a2e33c4786354749c0b9159ec56dbd6f10
Quan Pham <6479147+quanphm@users.noreply.github.com>
2025-10-06 15:07:59 +0700

    feat: add path for error message

--- packages/common/src/standard-validate.ts
+++ packages/common/src/standard-validate.ts
@@ -1,7 +1,9 @@
 import type { StandardSchemaV1 } from "@standard-schema/spec";
 
-export function createIssueMsg(issues: readonly { message: string }[]) {
-	const stdIssues = issues.map((issue) => issue.message);
+export function createIssueMsg(issues: readonly StandardSchemaV1.Issue[]) {
+	const stdIssues = issues.map(
+		(issue) => `${issue.message}${issue.path ? ` at ${issue.path}` : ""}`,
+	);
 	return stdIssues.length > 0 ? stdIssues[0] : "";
 }
 
@@ -15,7 +17,7 @@ export function standardValidate<T extends StandardSchemaV1>(
 	}
 	if (parsed.issues) {
 		const reducedIssues = createIssueMsg(parsed.issues);
-		throw new Error(`❌ Invalid environment variables: ${JSON.stringify(reducedIssues, null, 2)}`);
+		throw new Error(`❌ Invalid: ${JSON.stringify(reducedIssues, null, 2)}`);
 	}
 	return parsed.value;
 }
