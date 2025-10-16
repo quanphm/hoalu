b9a3827648db0b85f5756915618e4268747a7838
Quan Pham <6479147+quanphm@users.noreply.github.com>
2025-10-16 16:00:43 +0700

    feat: monday is week start day

--- packages/ui/src/components/calendar.tsx
+++ packages/ui/src/components/calendar.tsx
@@ -21,6 +21,7 @@ function Calendar({
 
 	return (
 		<DayPicker
+			weekStartsOn={1}
 			showOutsideDays={showOutsideDays}
 			className={cn(
 				"group/calendar bg-background p-3 [--cell-size:--spacing(8)] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent",
