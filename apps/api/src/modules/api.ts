cfdc2f24849c1d2e3607546db36f4f7f067522bc
Quan Pham <6479147+quanphm@users.noreply.github.com>
2025-12-14 15:34:59 +0700

    chore: release new version (#261)

--- apps/api/src/modules/api.ts
+++ apps/api/src/modules/api.ts
@@ -39,7 +39,7 @@ export function apiModule() {
 				info: {
 					title: "Hoalu API",
 					description: "OpenAPI documentation",
-					version: "0.16.0",
+					version: "0.17.0",
 				},
 				servers: [{ url: process.env.PUBLIC_API_URL }],
 			},
