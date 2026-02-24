7535ceddb8efebd708c16e18af36f0cc10aa6a5a
Quan Pham <6479147+quanphm@users.noreply.github.com>
2026-02-24 10:43:31 +0700

    chore: update ui

--- apps/app/src/components/command-palette/header-item.tsx
+++ apps/app/src/components/command-palette/header-item.tsx
@@ -8,7 +8,7 @@ export function HeaderItem({ label, style }: HeaderItemProps) {
 		<div
 			role="presentation"
 			aria-hidden="true"
-			className="text-muted-foreground absolute top-0 left-0 w-full px-2 py-2 text-xs font-medium"
+			className="text-muted-foreground absolute top-0 left-0 w-full px-2 py-2 text-xs font-semibold capitalize"
 			style={style}
 		>
 			{label}
