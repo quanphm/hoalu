85d2060aefff494b8f269ee18dbfd32b47c1c989
Quan Pham <6479147+quanphm@users.noreply.github.com>
2026-03-15 13:19:43 +0700

    Revert "chore: from router to start (#337)" (#339)
    This reverts commit eeec54b2ceb8f7f04d2803855c7f95a0597f4755.

--- apps/app/src/components/providers/tanstack-provider.tsx
+++
@@ -1,6 +0,0 @@
-import { queryClient } from "#app/lib/query-client.ts";
-import { QueryClientProvider } from "@tanstack/react-query";
-
-export default function TanStackQueryProvider(props: { children: React.ReactNode }) {
-	return <QueryClientProvider client={queryClient}>{props.children}</QueryClientProvider>;
-}
